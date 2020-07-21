74fee5f9aa3bfa1cdfa901488297a5b59cb4d9fd
Роберт Исмагилов <ledohod@icloud.com>
2020-07-21 21:29:54 +0300

    second edit

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,23 +1,9 @@
-/*
-const addTime = function() {
-    const timer = document.getElementById("timer");
-    timer.textContent -= 1;
-    if (timer.textContent < 0) { 
-        alert('Вы победили в конкурсе!');
-        return}
-    return
-}
 
-    setInterval(addTime, 100);
-*/
 const timer = document.getElementById("timer");
-
-let add = setInterval(addTime, 100);
-
-function addTime(){
+let add = setInterval(addTime, 1000);
+function addTime() {
     timer.textContent -= 1;
     if (timer.textContent == 0 ) { 
-        //timer.textContent = 0;
         alert('Вы победили в конкурсе!');
         clearInterval(add);
         }
