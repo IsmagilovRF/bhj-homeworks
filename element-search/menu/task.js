7f4eaecae04d762068a494cfdfd30861ef650cad
Роберт Исмагилов <ledohod@icloud.com>
2020-08-05 08:35:15 +0300

    first commit menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let links = document.getElementsByClassName('menu__link');// получили  элементы класса menu__link 
+
+let arrMenuLinks = Array.from(links);//превратили в массив
+
+for (let i = 0; i < arrMenuLinks.length; i++) {
+    const arrMenuLink = arrMenuLinks[i];
+    arrMenuLink.onclick = () => {
+       const nextElements = arrMenuLink.nextElementSibling;//следующий элемент меню
+       nextElements.classList.add("menu_active");//Добавляем класс: menu_active
+       return false
+    }
+}
