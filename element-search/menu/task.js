1b2d7eb7426f99a94da5812957d385299c527373
Роберт Исмагилов <ledohod@icloud.com>
2020-08-22 02:09:42 +0300

    second commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,12 @@
 'use strict';
 
 let links = document.getElementsByClassName('menu__link');// получили  элементы класса menu__link 
-
 let arrMenuLinks = Array.from(links);//превратили в массив
-
 for (let i = 0; i < arrMenuLinks.length; i++) {
     const arrMenuLink = arrMenuLinks[i];
     arrMenuLink.onclick = () => {
        const nextElements = arrMenuLink.nextElementSibling;//следующий элемент меню
-       nextElements.classList.add("menu_active");//Добавляем класс: menu_active
+       nextElements.classList.toggle("menu_active");//Добавляем класс: menu_active
        return false
     }
 }
