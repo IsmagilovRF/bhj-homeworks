7e941001a30920cc632b16e6b3a39ac0bbf815cf
Роберт Исмагилов <ledohod@icloud.com>
2020-08-22 02:01:34 +0300

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,39 +1,33 @@
 'use strict';
 
-const slides = document.getElementsByClassName('slider__item');//получили элементы класса slider__item (это все слайды)
-const arrSlides = Array.from(slides);//превратили в массив элементов
-
+let slides = document.getElementsByClassName('slider__item');//получили элементы класса slider__item (это все слайды)
+let arrSlides = Array.from(slides);//превратили в массив
+let len = arrSlides.length;
 const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 
 let position = 0;
-let i = 0;
+for (let k = 0; k < len; k++) {
+    next.onclick = function () { return function () {
+        arrSlides[position].classList.remove("slider__item_active");//убираем класс: slider__item_active
+        position = position + 1;
+        if (position >=len) {position = 0;}
+        arrSlides[position].classList.add("slider__item_active");//добавляем класс: slider__item_active
+    }(k);
+    };
+}
 
-arrSlides.forEach( function(item, i, arr) {
-    //console.log(i, item, arr);
-    next.onclick = function(){
-        position = position + i;
-        if (position >= arrSlides.length) {
-            position = 0;
-            //console.log('i = ' + i + 'position = ' + position    + '   position >= arrSlides.length');
-        } else {
-            //console.log(i + '   ' + position);
-            const arrSlide = arrSlides[position];
-            arrSlide.classList.add("slider__item_active");//Добавляем класс: slider__item_active
-        }
-    }
-    position = arrSlides.length
-    i = 1;
-    prev.onclick = function(){
-        position = position - i;
-        if (position < 0) {
-            console.log('position < 0');
-        } else {
-        console.log(i + '   ' + position);
-        const arrSlide = arrSlides[position];
-        arrSlide.classList.add("slider__item_active");//Добавляем класс: slider__item_active
-        }
-    }
-});
+let aposition = len - 1;
+for (let k = 0; k < len; k++) {
+    prev.onclick = function () { return function () {
+        arrSlides[aposition].classList.remove("slider__item_active");//убираем класс: slider__item_active
+        aposition = aposition - 1;
+        if (aposition < 0) {aposition = len -1;}
+        arrSlides[aposition].classList.add("slider__item_active");//добавляем класс: slider__item_active
+    }(k);
+    };
+    
+}
 
 
+  
